5148b9ca774181a450718a9835b876cee5664c5c
Shubham Jena <shubham2jena2003@gmail.com>
2024-12-05 20:46:55 +0530

    Initial commit

---
+++ src/main/java/ormanindia/orman/OrmanApplication.java
@@ -0,0 +1,13 @@
+package ormanindia.orman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ormanindia/orman/OrmanApplicationTests.java
@@ -0,0 +1,13 @@
+package ormanindia.orman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
